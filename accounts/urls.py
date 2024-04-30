43d49836b6a77d139c37f1dd3ba71b193eb37388
park <uyeongi@naver.com>
2024-04-30 23:05:28 +0900

    페이지 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,11 +6,11 @@ app_name = "accounts"
 
 urlpatterns = [
     path('', views.user_list, name='user_list'),
-    path('api/accounts/signup/', views.user_signup, name='user_signup'),
-    path('api/accounts/login/', views.user_login, name='user_login'),
-    path('api/accounts/logout/', views.user_logout, name='user_logout'),
+    path('signup/', views.user_signup, name='user_signup'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
     path('api/accounts/<str:username>/', views.user_profile, name='user_profile'),
-    path("signup/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("admin/signup/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     
     
